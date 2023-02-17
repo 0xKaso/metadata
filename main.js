7265efc7389e8687f418695b1c764fa0230ead1f
kasodesyn <bokang.qian@desyn.io>
2023-02-17 16:52:05 +0800

    i

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-
-var cryptoHash = require("crypto-hashing");
-
-// console.log(cryptoHash('hash256', new Buffer('Hello there!')).toString('hex'))
+const cryptoHash = require("crypto-hashing");
 
 fs.readFile(path.join(__dirname, "./data.txt"), (err, res) => {
   const arr = res
@@ -127,47 +124,12 @@ fs.readFile(path.join(__dirname, "./data.txt"), (err, res) => {
     return metadata;
   });
 
-  // const resString = res.toString();
-  // const resultJSON = resString.split(',').map(item => item.split('\t')).map(item => item.map(item => item.trim())).map(item => {
-  //     if (!item[0] || !item[1] || !item[2]) {
-  //         console.log('---提供的资源数值有误---');
-  //         isError = true;
-  //     }
-  //     return {
-  //         tokenId: item[0],
-  //         userAddress: item[1],
-  //         type: item[2],
-  //     }
-  // }).map(item => {
-  //     const dataTime = new Date().getTime()
-  //     const metadata = {
-  //         // description: tokenType[item.type].desc,
-  //         image: `https://raw.githubusercontent.com/W3T-Web3-Genius-Tools/sbt/main/src/metadata/images/${item.tokenId}.png`,
-  //         name: `${tokenType[item.type].tag} #${item.tokenId}`,
-  //         tokenId: item.tokenId,
-  //         external_url: "https://github.com/W3T-Web3-Genius-Tools",
-  //         attributes:
-  //             [{trait_type: 'Type of Honor', value: tokenType[item.type].tag},
-  //                 {trait_type: 'Award Address', value: item.userAddress},
-  //                 {trait_type: 'Signature Date', "display_type": "date", value: dataTime},
-  //                 {trait_type: 'Signatories', value: "0xf8D9d01c90B84dc99064968ED77b829Ab0A593f7"}]
-  //     }
-  //     return metadata
-  // })
-
-  // // 生成单文件-总文件
-  //   fs.writeFileSync(path.join(__dirname, './metadata.json'), JSON.stringify(metadata, null, 2))
-  //   fs.writeFileSync(path.join(__dirname, '../images-gen/src/data/data.json'), JSON.stringify(metadata, null, 2))
-  // 生成多文件-单文件
   for (let i = 0; i < metadata.length; i++) {
-    console.log(metadata[i]);
     fs.writeFileSync(
       path.join(__dirname, `./data/${metadata[i].tokenId}.json`),
       JSON.stringify(metadata[i], null, 2)
     );
   }
 
-  // if (!isError) {
-  //     console.log("解析成功");
-  // }
+  console.log("Success!")
 });
